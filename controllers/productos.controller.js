e18e664f311fcd4570278b1ff7dee9fd59a6fe9a
Jayzzon <gamboafrancisco44@gmail.com>
2023-07-21 13:42:59 -0600

    Se agrego otro controlador y se agrego a las rutas

--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -102,28 +102,5 @@ export default {
             message:"La consulta no pudo lograrse"
         })
     }
-  },
-  aggPerfil:async(req,res,next)=>{
-    try {
-      const { usuario, email, password } = req.body;
-
-      const agregarPerfil = new Models.Perfil({
-        usuario,
-        email,
-        password,
-      });
-
-      //agregarProducto.filename=req.file.filename;
-      //agregarProducto.path='public/images/'+req.file.filename;
-
-      const agregarP = await agregarPerfil.save();
-      res.status(200).json(agregarP);
-      console.log(req.body);
-    } catch (error) {
-      res.status(500).send({
-        message: "Ocurrio un error al guardar en la BD",
-      });
-      next(error);
-    }
   }
 };
--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -1,5 +1,6 @@
 import routerx from "express-promise-router";
 import productosController from "../controllers/productos.controller";
+import perfileControllers from "../controllers/perfile.controllers";
 
 const router=routerx();
 
@@ -9,6 +10,6 @@ router.get("/consultarProducto",productosController.consultarProducto);
 router.delete("/eliminarProducto/:id",productosController.eliminarProducto);
 router.patch("/updateProducto/:id",productosController.updateProducto);
 router.get("/oneProducto/:id",productosController.consultarOneProduct);
-router.post('/login',productosController.aggPerfil);
+router.post('/login',perfileControllers.aggPerfil);
 
 export default router;
